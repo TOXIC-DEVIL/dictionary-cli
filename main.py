bbdf493dca6065040ba1b4856a8e2d6a90872d8f
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2024-05-13 11:59:12 +0530

    add: main()

--- main.py
+++ main.py
@@ -14,12 +14,36 @@ def getword():
       if isinstance(arg, str) and arg.strip():
         break
       else:
-        print("Please enter a valid word.")
+        print('Please enter a valid word.')
     return arg.strip()
 
 def main():
-  getword()
-  # to-do
+  base_url = 'https://api.dictionaryapi.dev'
+  word = getword()
+
+  res = requests.get(f'{base_url}/api/v2/entries/en/{word}')
+  json = res.json()
+  if 'title' in json:
+    print(json['title'])
+    print(json['message'])
+  else:
+    json = json[0]
+    phonetics = []
+    meanings = []
+    for ph in json['phonetics']:
+      if 'text' in ph:
+        phonetics.append(ph['text'])
+    for meaning in json['meanings']:
+      meaning_data = 'Part of Speech: ' + meaning['partOfSpeech'] + '\n' + 'Definition: ' + meaning['definitions'][0]['definition'] + '\n' + 'Example: ' + meaning['definitions'][0]['example']
+      meanings.append(meaning_data)
+    print(
+      'Word: ' + word + '\n' +
+      'Phonetics: ' + ', '.join(phonetics) + '\n' +
+      '--------------------------------'
+    )
+    for meaning in meanings:
+      print(meaning)
+      print('--------------------------------')
 
 if __name__ == '__main__':
   main()
